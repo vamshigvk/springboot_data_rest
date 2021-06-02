6416a1d965ef1313f47efa616fc309aef0dba168
vamshi <vamshikrishnagvk5@gmail.com>
2021-06-02 22:19:59 +0530

    first

---
+++ src/test/java/com/vamshigvk/hellospringbootdatatest/HelloSpringbootDataRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vamshigvk.hellospringbootdatatest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpringbootDataRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
